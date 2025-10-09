ae2b6d7d9ca87c0d86231aefacea944c0535b1ed
reymondxtrm <reymondxtrm@gmail.com>
2025-10-09 14:57:07 +0800

    feat: implement ShouldQueue interface for notification classes

--- app/Notifications/AfterPaymentNotification.php
+++ app/Notifications/AfterPaymentNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class AfterPaymentNotification extends Notification
+class AfterPaymentNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
